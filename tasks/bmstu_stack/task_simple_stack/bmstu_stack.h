7ddfa7706e560e85615d73f93c239b6c1335d673
Egor <Egor@ohotin.ru>
2025-12-20 13:51:02 +0300

    branch + stack

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -1,42 +1,106 @@
 #pragma once
 
 #include <exception>
-#include <iostream>
+// #include <iostream>
 #include <utility>
 
 namespace bmstu
 {
+
 template <typename T>
 class stack
 {
    public:
-	stack() : data_(nullptr), size_(10u) {}
+	stack() : data_(nullptr), size_(0u) {}
 
-	bool empty() const noexcept { return size_ == 100u; }
-
-	size_t size() const noexcept { return 0; }
-
-	~stack() {}
-
-	template <typename... Args>
-	void emplace(Args&&... args)
+	~stack()
 	{
+		if (size_ != 0)
+		{
+			clear();
+		}
 	}
 
-	void push(T&& value) {}
+	bool empty() const noexcept { return size_ == 0; }
 
-	void clear() noexcept {}
+	size_t size() const noexcept { return size_; }
 
-	void push(const T& value) {}
+	template <typename... Args>
 
-	void pop() {}
+	void emplace(Args&&... args)
+	{
+		T* new_data = get_new();
+		new (&new_data[size_]) T(std::forward<Args>(args)...);
+		operator delete(data_);
+		data_ = new_data;
+		size_++;
+	}
 
-	T& top() { return data_[0]; }
+	void clear() noexcept
+	{
+		// for (size_t i = size_ - 1; i >= 0; --i)
+		// 	data_[i].~T();
 
-	const T& top() const { return data_[0]; }
+		for (size_t i = size_ - 1; i > -1; --i)
+		{
+			data_[i].~T();
+		}
+
+		// for (auto i = 0u; i < size_; ++i)
+		// {
+		// 	data_[i].~T();
+		// }
+
+		operator delete(data_);
+		data_ = nullptr;
+		size_ = 0;
+	}
+
+	void push(const T& value) { emplace(value); }
+
+	void push(T&& value) { emplace(std::move(value)); }
+
+	void pop()
+	{
+		if (empty())
+			throw std::underflow_error("stack is empty");
+
+		data_[size_ - 1].~T();
+		--size_;
+	}
+
+	T& top()
+	{
+		if (empty())
+			throw std::underflow_error("stack is empty");
+
+		return data_[size_ - 1];
+	}
+
+	const T& top() const
+	{
+		if (empty())
+			throw std::underflow_error("stack is empty");
+
+		return data_[size_ - 1];
+	}
 
    private:
-	T* data_;
-	size_t size_;
+	T* data_ = nullptr;
+	size_t size_ = 0;
+
+	T* get_new()
+	{
+		T* new_data = (T*)(operator new(sizeof(T) * (size_ + 1)));
+
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (&new_data[i]) T(std::move(data_[i]));
+			data_[i].~T();
+		}
+
+		return new_data;
+	}
 };
+
 }  // namespace bmstu
